427582fc0701cea14f9ca86b098ecec7b87eb4b1
Evan Candler <emcandler21@gmail.com>
2018-04-03 17:41:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,7 @@ def self.find_by_id(id)
 end
 
 def self.find_or_create_by(name:, breed:)
-  dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND album = ?", name, breed)
+  dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
   if !dog.empty?
     dog_data = dog[0] #grab dog data from the dog array of arrays
     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2]) #creates instance of dog if it doesn't already exist
