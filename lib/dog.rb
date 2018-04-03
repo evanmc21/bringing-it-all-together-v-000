2a61692b457614c5964a0576dafbb855046e6335
Evan Candler <emcandler21@gmail.com>
2018-04-03 17:17:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,4 +40,9 @@ class Dog
       self.new_from_db(row)
     end.first
   end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
