cc0e7efff9dc680b00709592f459483673c01e96
Evan Candler <emcandler21@gmail.com>
2018-04-03 17:19:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,4 +45,16 @@ class Dog
     sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
+
+  def save 
+    if self.id
+      self.update
+    else
+  sql = <<-SQL
+    INSERT INTO students (name, grade) VALUES (?, ?)
+    SQL
+
+  DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
 end
